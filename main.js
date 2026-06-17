da73f7328c1600823ab6405ae60a3703d3efe429
RamalheiraJV <ramalheira.joao@escola.pr.gov.br>
2026-06-17 20:41:44 -0300

    .

--- main.js
+++ main.js
@@ -1,19 +1,20 @@
 
-        const btn = document.getElementById('accessibilityBtn');
-        const menu = document.getElementById('accessibilityMenu');
 
-        btn.addEventListener('click', () => {
-            menu.classList.toggle('active');
-        });
+const btn = document.getElementById("accessibilityBtn");
+const menu = document.getElementById("accessibilityMenu");
 
-        let fontSize = 100;
+btn.addEventListener("click", () => {
+    menu.classList.toggle("active");
+});
 
-        function changeFontSize(value) {
-            fontSize += value * 10;
-            fontSize = Math.max(70, Math.min(fontSize, 200));
-            document.body.style.fontSize = fontSize + '%';
-        }
+let fontSize = 100;
 
-        function toggleContrast() {
-            document.body.classList.toggle('high-contrast');
-        }
+function changeFontSize(value) {
+    fontSize += value * 10;
+    fontSize = Math.max(80, Math.min(fontSize, 180));
+    document.body.style.fontSize = fontSize + "%";
+}
+
+function toggleContrast() {
+    document.body.classList.toggle("high-contrast");
+}
